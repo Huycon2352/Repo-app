735465f0ba9ca6667606418d432dab2cb5b35abf
Huycon2352 <156405801+Huycon2352@users.noreply.github.com>
2026-06-03 13:26:23 +0700

    Update test_app.py

--- vote/tests/test_app.py
+++ vote/tests/test_app.py
@@ -11,6 +11,40 @@
 #     response = client.get("/")
 #     assert response.status_code == 200
 
+# import sys
+# from pathlib import Path
+# from unittest.mock import MagicMock, patch
+
+# sys.path.insert(0, str(Path(__file__).resolve().parent.parent))
+
+# from app import app
+
+
+# def test_home():
+#     client = app.test_client()
+
+#     response = client.get("/")
+
+#     assert response.status_code == 200
+
+
+# @patch("app.get_redis")
+# def test_vote(mock_get_redis):
+#     mock_redis = MagicMock()
+#     mock_get_redis.return_value = mock_redis
+
+#     client = app.test_client()
+
+#     response = client.post(
+#         "/",
+#         data={"vote": "Cats"}
+#     )
+
+#     assert response.status_code == 200
+
+#     mock_redis.rpush.assert_called_once()
+
+
 import sys
 from pathlib import Path
 from unittest.mock import MagicMock, patch
@@ -37,9 +71,11 @@ def test_vote(mock_get_redis):
 
     response = client.post(
         "/",
-        data={"vote": "Cats"}
+        data={"vote": "Cats"},
+        follow_redirects=True
     )
 
     assert response.status_code == 200
 
+    mock_get_redis.assert_called_once()
     mock_redis.rpush.assert_called_once()
